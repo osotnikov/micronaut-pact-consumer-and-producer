406bd961d0f84106bb2c52e6323fe5cbd7094522
olegk.sotnikov <alex28_501@hotmail.com>
2021-04-14 17:46:00 +0100

    news-aggregator: ArticlesClient

--- news-aggregator/src/main/java/example/articles/service/client/ArticlesClient.java
+++ news-aggregator/src/main/java/example/articles/service/client/ArticlesClient.java
@@ -1,6 +1,25 @@
 package example.articles.service.client;
 
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+
+import javax.inject.Singleton;
+
+import static io.micronaut.http.HttpRequest.GET;
+
+@Singleton
 public class ArticlesClient {
 
+    private final RxHttpClient articlesClient;
+
+    public ArticlesClient(final @Client("${api.endpoint.articles.host}") RxHttpClient articlesClient) {
+        this.articlesClient = articlesClient;
+    }
+
+    public String getArticleContent(String key) {
+        return articlesClient.retrieve(
+                GET("/articles?key=" + key), String.class).blockingFirst();
+    }
 
 }
