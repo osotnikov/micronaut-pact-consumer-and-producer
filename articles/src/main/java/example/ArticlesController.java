f5b0d8f6fc6cb597a678e7a84b76e797fd8e6a4f
olegk.sotnikov <alex28_501@hotmail.com>
2021-04-14 15:53:58 +0100

    added provider, articles post and get

---
+++ articles/src/main/java/example/ArticlesController.java
@@ -0,0 +1,36 @@
+package example;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.validation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Controller("/")
+@Validated
+public class ArticlesController {
+
+    private final static Set<String> ALLOWED_KEYS = Set.of("latest", "most_popular");
+    private static Map<String, String> articlesRepository = new HashMap<>();
+
+    @Post(uri = "/articles", produces = MediaType.TEXT_PLAIN)
+    public HttpResponse postArticle(@NotBlank String key, String content) {
+        if(ALLOWED_KEYS.contains(key)){
+            articlesRepository.put(key, content);
+            return HttpResponse.ok();
+        } else {
+            return HttpResponse.badRequest();
+        }
+    }
+
+    @Get(uri = "/articles", produces = MediaType.TEXT_PLAIN)
+    public HttpResponse<String> getArticle(@NotBlank String key) {
+        return HttpResponse.ok(articlesRepository.get(key));
+    }
+}
